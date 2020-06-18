f64c537bdae5f73d10c5022a711f89a78fe0ded1
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2020-06-18 18:08:12 -0400

    Updates

---
+++ precache-manifest.be8222f322fd5b22fdf3eb3bef3b3b0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8170f877a46559cf1a3f49f278bcd5",
+    "url": "/react-checkers/index.html"
+  },
+  {
+    "revision": "6a8195b6bb4c8f1edc39",
+    "url": "/react-checkers/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d8e12febf8da5a54fda7",
+    "url": "/react-checkers/static/js/2.66aa98fb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-checkers/static/js/2.66aa98fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8195b6bb4c8f1edc39",
+    "url": "/react-checkers/static/js/main.e9db721f.chunk.js"
+  },
+  {
+    "revision": "aad2e8715619895acd02",
+    "url": "/react-checkers/static/js/runtime-main.cb7d1fc6.js"
+  }
+]);
